171706222731240dc41e243eb0c1c2de3522bfde
Pranav Kataria <72139550+PranavTheChampKataria@users.noreply.github.com>
2020-10-02 13:12:22 +0530

    Update faultycalculator.py

--- faultycalculator.py
+++ faultycalculator.py
@@ -1,13 +1,13 @@
-num1 = int(input("enter your first number: "))
+num1 = int(input("enter your first number: ")) '''user input''' 
 print(num1)
 
-op = input("enter your operation: ")
+op = input("enter your operation: ") '''user input'''
 print(op)
 
-num2 = int(input("enter your second number: "))
+num2 = int(input("enter your second number: ")) '''user input'''
 print(num2)
 
-if(num1 == 56 and op == "+" and num2 == 7):
+if(num1 == 56 and op == "+" and num2 == 7): '''checking for values'''
     result = "77"
     print(result)
 
